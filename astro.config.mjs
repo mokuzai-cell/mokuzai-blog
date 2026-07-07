92bba60fd4871a700e9e89d3f6b0ff79a2b9438e
mokuzai-cell <7525109@ed.tus.ac.jp>
2026-07-07 19:10:05 +0900

    多言語対応しました

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,27 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import expressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [expressiveCode(),mdx()],
   site: "https://mokuzai-cell.github.io",
   base: "blog",
   trailingSlash: "always",
+  markdown: {
+    shikiConfig: {
+      // 好きなテーマを選べます（例: github-dark, dracula, one-dark-pro など）
+      theme: 'one-dark-pro',
+      // 行番号を表示したい場合は有効に
+      wrap: true,
+    },
+  },
+  i18n:{
+    locales:["ja","en"],
+    defaultLocale:"ja",
+    routing:{prefixDefaultLocale: false},
+  }
 });
